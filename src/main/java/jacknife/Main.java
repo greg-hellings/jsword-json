1fd1a3ce6cecf7061e375d6f38e44498c361bc09
Greg Hellings <greg.hellings@gmail.com>
2014-01-12 06:55:35 +0000

    Dynamic host name support.

--- src/main/java/jacknife/Main.java
+++ src/main/java/jacknife/Main.java
@@ -7,15 +7,21 @@ import static io.undertow.Handlers.websocket;
 
 public class Main {
 	public static void main(String[] args) {
+		String host = "localhost";
+		if (args.length > 1) {
+			host = args[1];
+		}
+        System.out.println("Binding to " + host);
+
 		SwordHandler handler = new SwordHandler();
 		
 		Undertow server = Undertow.builder()
-				.addListener(10001, "localhost")
+				.addListener(10001, host)
 				.setHandler(handler)
 				.build();
 		
 		Undertow socketServer = Undertow.builder()
-				.addListener(10002, "localhost")
+				.addListener(10002, host)
 				.setHandler(path().addPrefixPath("/", websocket(handler)))
 				.build();
 		
